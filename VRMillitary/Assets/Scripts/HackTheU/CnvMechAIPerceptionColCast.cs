d387a6739750ae9dec42b9a3862fa267b68d04ea
nwy140 <nwy140@gmail.com>
2019-10-14 17:26:12 -0600

    Slight tweaks

--- VRMillitary/Assets/Scripts/HackTheU/CnvMechAIPerceptionColCast.cs
+++ VRMillitary/Assets/Scripts/HackTheU/CnvMechAIPerceptionColCast.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PerceptionColCast : MonoBehaviour
+public class CnvMechAIPerceptionColCast : MonoBehaviour
 {
     public GameObject collidedObj;
 
